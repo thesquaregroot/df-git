73a0aa7d25483639f7eb2f01e5436f54d8548ccd
Andrew Groot <groot.andrew@gmail.com>
2014-12-21 19:39:37 -0500

    Path to df-git.sh is no longer relative.

--- df-start.sh
+++ df-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get newest changes
-./df-git.sh pull
+df-git.sh pull
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
@@ -10,10 +10,10 @@ fi
 dwarffortress
 
 # auto-commit
-./df-git.sh commit "[$(date +"%Y-%m-%d %H:%M:%S")] auto-commit"
+df-git.sh commit "[$(date +"%Y-%m-%d %H:%M:%S")] auto-commit"
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
 # update repo
-./df-git.sh push
+df-git.sh push
 
