d0d7407e922135f2660b805bac9a3ed1ec30cfe8
Andrew Groot <groot.andrew@gmail.com>
2015-08-09 15:40:31 -0400

    Tracking install directory for save files.

--- df-git.sh
+++ df-git.sh
@@ -9,13 +9,23 @@ function print_usage() {
     echo "  force-state                 Overwrites dwarf fortress save files with"
     echo "                                  repository state"
     echo "  get-binary-path             Returns the configured binary path (see setup)"
+    echo "  get-install-path            Returns the configured install path (see setup)"
     echo "  help                        Prints this help screen"
     echo "  pull                        Updates the repository with remote changes"
     echo "  push                        Uploads changes to the remote repository"
-    echo "  setup <df-binary-path>      Configure Dwarf Fortress binary location."
-    echo "                                  This will be stored in ~/.df-git-binary-path"
+    echo "  setup [...]                 See configuration commands below."
     echo "  upgrade-config              Re-creates the dwarf fortress directory and"
-    echo "                                  re-installs save files"
+    echo "                                  re-installs save files.  WARNING: this"
+    echo "                                  will only work in Arch Linux!"
+    echo
+    echo "Command for configuring df-git:"
+    echo "  setup                       When used without arguments, prints configuration."
+    echo "  setup install <path>        Configure Dwarf Fortress installation directory"
+    echo "                                  This will be stored in ~/.df-git-install-path"
+    echo "  setup binary <path>         Configure Dwarf Fortress binary location."
+    echo "                                  This will be stored in ~/.df-git-binary-path"
+    echo "  setup clear                 Removes df-git configuration files (but not"
+    echo "                                  ~/.df-git/)."
     echo
     echo "Commands 'forwarded' to git (normal git arguments can be used):"
     echo "  branch"
@@ -36,30 +46,67 @@ fi
 
 # Determine installation directory
 DF_BINARY_PATH_FILE="${HOME}/.df-git-binary-path"
+DF_INSTALL_PATH_FILE="${HOME}/.df-git-install-path"
 function setup_binary_path() {
     path="$1"
-    if [[ ! -f $path ]]; then
-        echo "$path is not a file."
+    if [[ ! -f "$path" ]]; then
+        echo "ERROR: $path is not a file."
     else
-        if [[ -x $path ]]; then
+        if [[ -x "$path" ]]; then
             echo "$path" > $DF_BINARY_PATH_FILE
+            echo "Binary path set to $path."
         else
             echo "$path is not an executable file."
         fi
     fi
 }
+function setup_install_path() {
+    path="$1"
+    if [[ ! -d "$path" ]]; then
+        echo "ERROR: $path is not a directory."
+    else
+        save_path="$path/data/save/"
+        if [[ ! -d "$save_path" ]]; then
+            echo "WARNING: Could not find $save_path."
+        else
+            echo "$path" > $DF_INSTALL_PATH_FILE
+            echo "Install path set to $path."
+            default_binary="$path/df"
+            if [[ -f "$default_binary" && -x "$default_binary" ]]; then
+                setup_binary_path $default_binary
+            fi
+        fi
+    fi
+}
+# check for install
+if [[ -e $DF_INSTALL_PATH_FILE ]]; then
+    DF_DIR=$(cat $DF_INSTALL_PATH_FILE)
+else
+    ARCH_INSTALL="${HOME}/.dwarffortress/"
+    if [[ -d $ARCH_INSTALL ]]; then
+        echo "Found $ARCH_INSTALL, configuring install path..."
+        setup_install_path "$ARCH_INSTALL"
+        DF_DIR="$ARCH_INSTALL"
+    else
+        echo "Could not find Dwarf Fortress installation path."
+        echo "Please configure binary location using:"
+        echo "  $0 setup install <df-install-path>"
+        exit 1
+    fi
+fi
+# check for binary
 if [[ -e $DF_BINARY_PATH_FILE ]]; then
     DF_BIN=$(cat $DF_BINARY_PATH_FILE)
 else
     ARCH_BIN="/usr/bin/dwarffortress"
     if [[ -f $ARCH_BIN && -x $ARCH_BIN ]]; then
-        echo "Found $ARCH_BIN, configuring df-git..."
+        echo "Found $ARCH_BIN, configuring binary path..."
         setup_binary_path $ARCH_BIN
         DF_BIN="$ARCH_BIN"
     else
         echo "Could not find Dwarf Fortress binary."
         echo "Please configure binary location using:"
-        echo "  $0 setup <df-binary-path>"
+        echo "  $0 setup binary <df-binary-path>"
         exit 1
     fi
 fi
@@ -67,8 +114,6 @@ fi
 ##
 ## Variables, aliases, and helper functions
 ##
-DF_DIR_NAME=".dwarffortress"
-DF_DIR="${HOME}/${DF_DIR_NAME}/"
 DF_GIT_DIR_NAME=".df-git"
 DF_GIT_DIR="${HOME}/${DF_GIT_DIR_NAME}/"
 alias cp="cp -v"
@@ -86,7 +131,15 @@ function remove_df_git_dir() {
         rm -rf "${DF_GIT_DIR}"
     fi
 }
-function copy_files() {
+function print_df_git_config() {
+    echo "Install path: $DF_DIR"
+    echo "Binary path:  $DF_BIN"
+}
+function remove_df_git_config_files() {
+    rm -v "$DF_INSTALL_PATH_FILE"
+    rm -v "$DF_BINARY_PATH_FILE"
+}
+function copy_save_files() {
     src_dir="$1"
     dest_dir="$2"
     # determine if rysnc is installed
@@ -104,11 +157,11 @@ function copy_files() {
 }
 function install_df_git_files() {
     echo "Installing df-git files..."
-    copy_files "${DF_GIT_DIR}" "${DF_DIR}"
+    copy_save_files "${DF_GIT_DIR}" "${DF_DIR}"
 }
 function update_df_git_files() {
     echo "Updating df-git files..."
-    copy_files "${DF_DIR}" "${DF_GIT_DIR}"
+    copy_save_files "${DF_DIR}" "${DF_GIT_DIR}"
 }
 function confirm_update() {
     read -p "Remote changes found, are you sure you want to overwrite your local Dwarf Fortress save files? [y/N] " response
@@ -178,7 +231,14 @@ case "$cmd" in
         echo "Usage: $0 $cmd"
         exit 1
     fi
-    echo $DF_BIN
+    echo "$DF_BIN"
+    ;;
+"get-install-path")
+    if [[ $# -ne 1 ]]; then
+        echo "Usage: $0 $cmd"
+        exit 1
+    fi
+    echo "$DF_DIR"
     ;;
 # PULL UPDATES FROM CURRENT BRANCH
 "pull")
@@ -209,13 +269,46 @@ case "$cmd" in
     ;;
 # SETUP DWARF FORTRESS BINARY LOCATION
 "setup")
-    if [[ $# -ne 2 ]]; then
-        echo "Usage $0 $cmd <df-binary-path>"
-        exit 1
+    setup_cmd="$2"
+    if [[ -z "$2" ]]; then
+        print_df_git_config
+    else
+        case "$setup_cmd" in
+        # setup install path
+        "install")
+            if [[ $# -ne 3 ]]; then
+                echo "Usage $0 $cmd $setup_cmd <df-install-path>"
+                exit 1
+            fi
+            install_path="$3"
+            echo "Configuring install path ${install_path}..."
+            setup_install_path "$install_path"
+            ;;
+        # setup binary path
+        "binary")
+            if [[ $# -ne 3 ]]; then
+                echo "Usage $0 $cmd $setup_cmd <df-binary-path>"
+                exit 1
+            fi
+            binary_path="$3"
+            echo "Configuating binary path ${binary_path}..."
+            setup_binary_path "$binary_path"
+            ;;
+        # remove existing configuration
+        "clear")
+            if [[ $# -ne 2 ]]; then
+                echo "Usage $0 $cmd $setup_cmd"
+                exit 1
+            fi
+            remove_df_git_config_files
+            ;;
+        *)
+            echo "Unrecognized setup command: $setup_cmd"
+            print_usage
+            exit 1
+            ;;
+        esac
     fi
-    binary_path="$2"
-    echo "Configuation binary path $binary_path..."
-    setup_binary_path $binary_path
     ;;
 # RECREATE DWARF FORTRESS CONFIGURATION
 "upgrade-config")
